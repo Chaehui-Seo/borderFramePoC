979379b87e18a866ec31ffad07e00261a366a05e
Chaehui Seo <sch0991@naver.com>
2024-07-13 18:05:40 +0900

    코드 정리 및 샘플 뷰 추가

--- borderFramePOC/ViewController.swift
+++ borderFramePOC/ViewController.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private lazy var targetView = makeTargetView()
-    private lazy var secondTargetView = makeTargetView()
-    
+    private lazy var innerBorderView = makeView()
+    private lazy var centerBorderView = makeView()
+    private lazy var outerBorderView = makeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,96 +23,36 @@ class ViewController: UIViewController {
      */
     func setupUI() {
         makeGrid()
-        self.view.addSubview(targetView)
-        self.view.addSubview(secondTargetView)
+        
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 60
+        stackView.axis = .vertical
+        stackView.addArrangedSubview(innerBorderView)
+        stackView.addArrangedSubview(centerBorderView)
+        stackView.addArrangedSubview(outerBorderView)
+        
+        self.view.addSubview(stackView)
+        
         NSLayoutConstraint.activate([
-            targetView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
-            targetView.widthAnchor.constraint(equalToConstant: 100),
-            targetView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
-            targetView.heightAnchor.constraint(equalToConstant: 100),
-            secondTargetView.leadingAnchor.constraint(equalTo: self.targetView.trailingAnchor, constant: 50),
-            secondTargetView.widthAnchor.constraint(equalToConstant: 100),
-            secondTargetView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 400),
-            secondTargetView.heightAnchor.constraint(equalToConstant: 100),
+            stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            innerBorderView.heightAnchor.constraint(equalToConstant: 100),
+            innerBorderView.widthAnchor.constraint(equalToConstant: 100),
+            centerBorderView.heightAnchor.constraint(equalToConstant: 100),
+            centerBorderView.widthAnchor.constraint(equalToConstant: 100),
+            outerBorderView.heightAnchor.constraint(equalToConstant: 100),
+            outerBorderView.widthAnchor.constraint(equalToConstant: 100)
         ])
         self.view.layoutIfNeeded()
         
-//        let borderLayer = CALayer()
-//        let borderWidth: CGFloat = 10
-//        let borderFrame = CGRect(x: -(borderWidth / 2), y: -(borderWidth / 2),
-//                                 width: targetView.frame.size.width + borderWidth,
-//                                 height: targetView.frame.size.height + borderWidth)
-//        borderLayer.frame = borderFrame
-//        borderLayer.backgroundColor = UIColor.clear.cgColor
-//        borderLayer.borderWidth = borderWidth
-//        borderLayer.borderColor = UIColor.red.withAlphaComponent(0.5).cgColor
-//        borderLayer.cornerRadius = 20
-//        targetView.layer.addSublayer(borderLayer)
-//        
-//        
-//        let borderLayer2 = CALayer()
-//        let borderWidth2: CGFloat = 10
-//        let borderFrame2 = CGRect(x: -(borderWidth2), y: -(borderWidth2),
-//                                 width: secondTargetView.frame.size.width + (borderWidth2 * 2),
-//                                 height: secondTargetView.frame.size.height + (borderWidth2 * 2))
-//        borderLayer2.frame = borderFrame2
-//        borderLayer2.backgroundColor = UIColor.clear.cgColor
-//        borderLayer2.borderWidth = borderWidth2
-//        borderLayer2.borderColor = UIColor.red.withAlphaComponent(0.5).cgColor
-//        borderLayer2.cornerRadius = 20
-//        secondTargetView.layer.addSublayer(borderLayer2)
-//        
-//        targetView.layer.cornerRadius = 20
-//        secondTargetView.layer.cornerRadius = 20
-        
-//        targetView.layer.borderColor = UIColor.red.withAlphaComponent(0.5).cgColor
-//        targetView.layer.borderWidth = 5
-//        targetView.layer.cornerRadius = 10
-//        targetView.layer.masksToBounds = true
-//        self.view.layoutIfNeeded()
-//        let layer = CAShapeLayer()
-//        layer.path = targetView.getBezierPath()
-//        layer.fillColor = UIColor.red.cgColor
-//        targetView.layer.addSublayer(layer)
-        
-        targetView.setBorder(borderWidth: 10, borderColor: UIColor.red.withAlphaComponent(0.5).cgColor, cornerRadius: 20, type: .outside)
-        secondTargetView.setBorder(borderWidth: 10, borderColor: UIColor.red.withAlphaComponent(0.5).cgColor, cornerRadius: 0, type: .inside)
-        
-//        // Outside
-//        let borderLayer = CALayer()
-//        let borderWidth: CGFloat = 5
-//        let borderFrame = CGRect(x: -(borderWidth), y: -(borderWidth),
-//                                 width: targetView.frame.size.width + (borderWidth * 2),
-//                                 height: targetView.frame.size.height + (borderWidth * 2))
-//        borderLayer.frame = borderFrame
-//        borderLayer.backgroundColor = UIColor.clear.cgColor
-//        borderLayer.borderWidth = borderWidth
-//        borderLayer.borderColor = UIColor.black.cgColor
-//        targetView.layer.addSublayer(borderLayer)
-//        
-//// Center
-//let borderLayer = CALayer()
-//let borderWidth: CGFloat = 5
-//let borderFrame = CGRect(x: -(borderWidth / 2), y: -(borderWidth / 2),
-//                         width: targetView.frame.size.width + borderWidth,
-//                         height: targetView.frame.size.height + borderWidth)
-//borderLayer.frame = borderFrame
-//borderLayer.backgroundColor = UIColor.clear.cgColor
-//borderLayer.borderWidth = borderWidth
-//borderLayer.borderColor = UIColor.black.cgColor
-//targetView.layer.addSublayer(borderLayer)
-//        borderLayer.cornerRadius = targetView.layer.cornerRadius
-//        borderLayer.maskedCorners = [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
-//        let testLayer = CAShapeLayer()
-//        testLayer.frame = CGRect(x: -3, y: -3, width: targetView.frame.size.width + 3, height: targetView.frame.size.height + 3)
-//        testLayer.backgroundColor = UIColor.yellow.withAlphaComponent(0.5).cgColor
-//        testLayer.cornerRadius = 10
-//        testLayer.fillRule = .evenOdd
-////        borderLayer.mask = testLayer
-////        targetView.layer.mask = testLayer
-//        targetView.layer.cornerRadius = 10
+        innerBorderView.setBorder(borderWidth: 10, borderColor: UIColor.red.withAlphaComponent(0.5).cgColor, cornerRadius: 15, type: .inside)
+        centerBorderView.setBorder(borderWidth: 10, borderColor: UIColor.red.withAlphaComponent(0.5).cgColor, cornerRadius: 15, type: .center)
+        outerBorderView.setBorder(borderWidth: 10, borderColor: UIColor.red.withAlphaComponent(0.5).cgColor, cornerRadius: 15, type: .outside)
     }
-    
+}
+
+extension ViewController {
     /**
      Make gridlines whose spacings are 10
      */
@@ -150,7 +90,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func makeTargetView() -> UIView {
+    func makeView() -> UIView {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .blue
